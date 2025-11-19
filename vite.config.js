46732ba9ef4e40c58eec413a6f34b2cede93df47
Ayaz Altaf <Ayazaltaf5252@gmail.com>
2025-11-20 04:57:43 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
-  plugins: [],
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
 })
