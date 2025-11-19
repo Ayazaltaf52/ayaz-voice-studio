508590e699cc919edccfbc713464ad18e7fb0ad1
Ayaz Altaf <Ayazaltaf5252@gmail.com>
2025-11-20 03:40:44 +0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VoiceGeneratorApp from './voice_generator'
+import './index.css'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VoiceGeneratorApp />
+  </React.StrictMode>,
+)
